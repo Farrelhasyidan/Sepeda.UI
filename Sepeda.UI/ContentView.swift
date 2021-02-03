08a82d370befbd478053b153989be1a4fe84f462
Farrelhasyidan <farrelhasyidan@gmail.com>
2021-02-03 11:45:46 +0700

    Menambahkan produk

--- Sepeda.UI/ContentView.swift
+++ Sepeda.UI/ContentView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+// model data
+struct ProductModel: Identifiable {
+    let id: Int
+    let namaProduk: String
+    let fotoProduk: String
+    let hargaProduk: Int
+    let lokasi: String
+    let ratingCount: Int
+    let jumlahRating: Int
+    
+    init(id: Int, namaProduk: String, fotoProduk: String, hargaProduk: Int, lokasi: String, ratingCount: Int, jumlahRating: Int){
+        
+        self.id = id
+        self.namaProduk = namaProduk
+        self.fotoProduk = fotoProduk
+        self.hargaProduk = hargaProduk
+        self.lokasi = lokasi
+        self.ratingCount = ratingCount
+        self.jumlahRating = jumlahRating
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Product()
     }
 }
 
@@ -18,4 +39,69 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Product: View {
+    var body: some View{
+        VStack(alignment: .leading){
+            //foto
+            ZStack(alignment: .topTrailing){
+                Image("foto1")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(height:250)
+                    .clipped()
+                
+                Button(action: {print("OK")}){
+                    Image(systemName: "heart")
+                        .padding()
+                        .foregroundColor(Color.red)
+                }
+            }
+            
+            Text ("Sepeda Polygon")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+                .padding(.leading)
+                .padding(.trailing)
+            Text ("Rp. 2.000.000")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.red)
+                .bold()
+                .padding(.leading)
+                .padding(.trailing)
+            
+            HStack{
+                Image(systemName: "strar fill")
+                    .foregroundColor(Color.yellow)
+                Image(systemName: "strar fill")
+                    .foregroundColor(Color.yellow)
+                Image(systemName: "strar fill")
+                    .foregroundColor(Color.yellow)
+            }
+            padding(.leading)
+            padding(.trailing)
+            
+            Button(action: {print("ditambahkan")}){
+                HStack{
+                    Spacer()
+                    HStack{
+                        Image(systemName: "cart")
+                        Text("Tambah ke keranjang")
+                            .font(.callout)
+                            .padding()
+                    }
+                    Spacer()
+                }
+            }
+            .background(Color.green)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+            .padding()
+        }
+        .background(Color("warna"))
+        .cornerRadius(15)
+        
+    }
+    
 }
